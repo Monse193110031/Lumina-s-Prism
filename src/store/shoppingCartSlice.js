4c6a6d94bb538b13486d87ff0fda7401401142f3
Monse Wininther <monse17j@gmail.com>
2023-06-30 11:10:44 -0600

    quantity control

--- src/store/shoppingCartSlice.js
+++ src/store/shoppingCartSlice.js
@@ -10,7 +10,8 @@ export const counterSlice = createSlice({
   reducers: {
     increment: (state, action) => {
       console.log(action.payload);
-      state.value.push(action.payload);
+      const elements = state.value.filter((item) => item.id === action.payload.id);
+      if (elements.length < action.payload.stock) state.value.push(action.payload);
     },
     decrement: (state, action) => {
       const element = state.value.findIndex((item) => item.id === action.payload.id);
@@ -22,14 +23,9 @@ export const counterSlice = createSlice({
     },
   },
 });
-// Action creators are generated for each case reducer function
 export const { increment, decrement, purchase } = counterSlice.actions;
 export const selectCount = (state) => state.counter.value?.length;
-export const selectTotal = (state) =>
-  state.counter.value.reduce((acc, item) => {
-    return acc + item.price;
-  }, 0);
-
+export const selectTotal = (state) => state.counter.value.reduce((acc, item) => acc + item.price, 0);
 export const selectProductsTotals = (state) =>
   state.counter.value.reduce((acc, item) => {
     if (!acc[item.id]) {
@@ -41,6 +37,10 @@ export const selectProductsTotals = (state) =>
     acc[item.id].quantity += 1;
     return acc;
   }, {});
+export const canAdd = (state, id) => {
+  const product = state.counter.value.find((item) => item.id === id);
+  return product?.quantity < product?.stock;
+};
 
 export const selectProducts = (state) => state.counter.value;
 
